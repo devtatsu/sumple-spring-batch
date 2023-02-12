67160cd87fb881cbd223d8bfdf784152da7e92e8
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2023-02-12 17:11:50 +0900

    add db credential information

--- demo/src/main/java/com/example/demo/chunk/processor/TestProcessor.java
+++ demo/src/main/java/com/example/demo/chunk/processor/TestProcessor.java
@@ -3,12 +3,10 @@ package com.example.demo.chunk.processor;
 import java.util.List;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 
 public class TestProcessor implements ItemProcessor<String, List<String>> {
 
     @Override
-    @Nullable
     public List<String> process(@NonNull String item) throws Exception {
         // TODO Auto-generated method stub
         return null;
--- demo/src/main/java/com/example/demo/chunk/reader/TestReader.java
+++ demo/src/main/java/com/example/demo/chunk/reader/TestReader.java
@@ -4,12 +4,10 @@ import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.item.support.AbstractItemStreamItemReader;
-import org.springframework.lang.Nullable;
 
 public class TestReader extends AbstractItemStreamItemReader<String> {
 
     @Override
-    @Nullable
     public String read() throws Exception, UnexpectedInputException, ParseException,
             NonTransientResourceException {
         // TODO Auto-generated method stub
